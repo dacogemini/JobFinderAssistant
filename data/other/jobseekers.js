72e8d4c43b0901e2e2a9b29773857f65c169185b
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-03-27 23:51:39 -0500

    added job search people object added site graphics

---
+++ data/other/jobseekers.js
@@ -0,0 +1,22 @@
+var seekerArray = [
+{
+  "name":"Bob",
+  "photo":"/img/bob.png"
+},{
+  "name":"Hanz",
+  "photo":"/img/hanz.png"
+},{
+  "name":"Horatio",
+  "photo":"/img/horatio.png"
+},{
+  "name":"Jesse",
+  "photo":"/img/jesse.png"
+},{
+  "name":"Prudence",
+  "photo":"/img/prudence.png"
+},{
+  "name":"Rick",
+  "photo":"/img/rick.png"
+}
+];
+module.exports = seekerArray;
