700604402bd4e0f33afbd0d2f138ff404824356e
Nick Volkert <nick@nickvolkert.com>
2018-03-28 20:16:11 -0500

    job choice page is done

--- data/other/jobseekers.js
+++ data/other/jobseekers.js
@@ -1,22 +1,34 @@
 var seekerArray = [
 {
   "name":"Bob",
-  "photo":"/img/bob.png"
+  "photo":"/img/bob.png",
+  "member-since": "2018",
+  "jobs-applied": "4"
 },{
   "name":"Hanz",
-  "photo":"/img/hanz.png"
+  "photo":"/img/hanz.png",
+  "member-since": "2016",
+  "jobs-applied": "7"
 },{
   "name":"Horatio",
-  "photo":"/img/horatio.png"
+  "photo":"/img/horatio.png",
+  "member-since": "2018",
+  "jobs-applied": "10"
 },{
   "name":"Jesse",
-  "photo":"/img/jesse.png"
+  "photo":"/img/jesse.png",
+  "member-since": "2018",
+  "jobs-applied": "20"
 },{
   "name":"Prudence",
-  "photo":"/img/prudence.png"
+  "photo":"/img/prudence.png",
+  "member-since": "2015",
+  "jobs-applied": "24"
 },{
   "name":"Rick",
-  "photo":"/img/rick.png"
+  "photo":"/img/rick.png",
+  "member-since": "2016",
+  "jobs-applied": "4"
 }
 ];
 module.exports = seekerArray;
